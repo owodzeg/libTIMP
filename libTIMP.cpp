612d92a9753a4ef3da00b1f99736842b0b4b7999
owocek <owocektv@gmail.com>
2021-03-14 22:48:04 +0100

    majority of textures work now

--- libTIMP.cpp
+++ libTIMP.cpp
@@ -19,26 +19,7 @@ void libTIMP::load(std::string file)
     TIMP.seekg(0x14);
     TIMP.read(reinterpret_cast<char*>(&height), sizeof(uint16_t));
 
-    uint8_t pal_type;
     int pal_colors = 16;
-
-    TIMP.seekg(0x19);
-    TIMP.read(reinterpret_cast<char*>(&pal_type), sizeof(uint8_t));
-
-    if(pal_type == 4)
-    {
-        cout << "16 color TIMP" << endl;
-        pal_colors = 16;
-        chunk_w = 32;
-        chunk_h = 8;
-    }
-    else if(pal_type == 5)
-    {
-        cout << "256 color TIMP" << endl;
-        pal_colors = 256;
-        chunk_w = 16;
-        chunk_h = 8;
-    }
 
     uint32_t pal_data,px_data;
 
@@ -47,6 +28,61 @@ void libTIMP::load(std::string file)
 
     TIMP.seekg(0x24);
     TIMP.read(reinterpret_cast<char*>(&px_data), sizeof(uint32_t));
+
+    ///pal_data == 0 when palette is not defined
+    if(pal_data != 0x0)
+    {
+        pal_colors = (px_data - 0x30) / 0x4;
+        cout << pal_colors << " color TIMP" << endl;
+    }
+    else
+    {
+        pal_colors = 0;
+    }
+
+    /*int w = floor(width / 16);
+    int h = floor(height / 16);
+
+    if(w%16 != 0)
+    {
+        float l = floor(log2(w*16));
+        float nw = pow(2,l+1);
+        width = nw;
+
+        cout << w << " " << l << " " << nw << endl;
+    }
+
+    if(h%16 != 0)
+    {
+        float l = floor(log2(h*16));
+        float nh = pow(2,l+1);
+        height = nh;
+
+        cout << h << " " << l << " " << nh << endl;
+    }*/
+
+    //system("pause");
+
+    if(pal_colors == 16)
+    {
+        chunk_w = 32;
+        chunk_h = 8;
+    }
+    else if(pal_colors == 256)
+    {
+        chunk_w = 16;
+        chunk_h = 8;
+    }
+    else if(pal_colors == 48)
+    {
+        chunk_w = 32;
+        chunk_h = 8;
+    }
+    else if(pal_colors == 0)
+    {
+        chunk_w = 4;
+        chunk_h = 8;
+    }
 
     for(int i=0; i<pal_colors; i++)
     {
@@ -69,21 +105,48 @@ void libTIMP::load(std::string file)
 
     int px_length = width*height;
 
-    for(int i=0; i<px_length; i++)
+    if(pal_data != 0x0)
     {
-        int pixel;
-
-        TIMP.seekg(px_data+i);
-        TIMP.read(reinterpret_cast<char*>(&pixel), sizeof(uint8_t));
-
-        if(pal_type == 5)
+        for(int i=0; i<px_length; i++)
         {
-            pixels.push_back(pixel);
+            int pixel;
+
+            TIMP.seekg(px_data+i);
+            TIMP.read(reinterpret_cast<char*>(&pixel), sizeof(uint8_t));
+
+            if(pal_colors == 256)
+            {
+                pixels.push_back(pixel);
+            }
+            else if((pal_colors == 16) || (pal_colors == 48))
+            {
+                pixels.push_back(pixel&0xF);
+                pixels.push_back((pixel>>4)&0xF);
+            }
         }
-        else if(pal_type == 4)
+    }
+    else
+    {
+        for(int i=0; i<px_length; i++)
         {
-            pixels.push_back(pixel&0xF);
-            pixels.push_back((pixel>>4)&0xF);
+            uint32_t pixel;
+
+            uint8_t r,g,b,a;
+
+            TIMP.seekg(px_data + i*4);
+            TIMP.read(reinterpret_cast<char*>(&r), sizeof(uint8_t));
+
+            TIMP.seekg(px_data + i*4 + 1);
+            TIMP.read(reinterpret_cast<char*>(&g), sizeof(uint8_t));
+
+            TIMP.seekg(px_data + i*4 + 2);
+            TIMP.read(reinterpret_cast<char*>(&b), sizeof(uint8_t));
+
+            TIMP.seekg(px_data + i*4 + 3);
+            TIMP.read(reinterpret_cast<char*>(&a), sizeof(uint8_t));
+
+            pal_color.push_back(sf::Color(r,g,b,a));
+            pixels.push_back(pal_color.size()-1);
         }
     }
 
@@ -108,8 +171,10 @@ sf::Image libTIMP::convertToImage()
                 if(y+ch < height)
                 {
                     image.setPixel(x+cw,y+ch,pal_color[pixels[p]]);
-                    p++;
+
                 }
+
+                p++;
             }
         }
 
